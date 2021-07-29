4789ba76f5236af93ee6a447fce4aa6a977a6cf0
Bart63 <bartek.durys@gmail.com>
2021-07-29 16:35:00 +0200

    Base component structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeButtonComponent } from './components/home-button/home-button.component';
+import { FormComponent } from './components/form/form.component';
+import { TableComponent } from './components/table/table.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeButtonComponent,
+    FormComponent,
+    TableComponent,
+    ChartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
