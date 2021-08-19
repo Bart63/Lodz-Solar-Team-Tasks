67259bca7a1b7427a84ea79483698b0b03456976
Bart63 <bartek.durys@gmail.com>
2021-08-19 21:22:11 +0200

    Mock repo, model, controller

---
+++ backend/Controllers/MeasurementsController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    //api/measurments
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeasurementsController : ControllerBase
+    {
+        private readonly MockRepo _repo = new MockRepo();
+
+        [HttpGet]
+        public ActionResult <IEnumerable<Measurement>> GetAllMeasurements()
+        {
+            var items = _repo.GetMeasurements();
+            return Ok(items);
+        }
+    }
+}
